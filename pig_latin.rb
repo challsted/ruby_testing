8d65b18ff6a9994cd1d0b2414b6193f3a366fd37
Chase Hallsted <chase.hallsted@gmail.com>
2016-08-14 21:41:25 -0500

    Simplified

--- pig_latin.rb
+++ pig_latin.rb
@@ -1,27 +1,24 @@
 #!/usr/bin/env ruby
 
-require 'colorize'
+require 'colorize' # Allows the use of colorized text to be printed to the screen
+# Program output           = Red
+# Output from User's Input = Yellow
+# User Input               = Terminal Default
 
 # This program will convert a given string to Pig Laten
-puts "\n\nPlease give me a word, I will convert it to Pig Latin!\n\n".red
+# TODO: Need to make sure that it is ONLY 1 word
 
-# Get the word from the user and ensure it has no capitals
+puts "\n\nPlease give me a word, I will convert it to Pig Latin!\n\n".red
 user_input = gets.chomp.downcase
 
 # Complain if the user didnt enter anything
-if user_input.empty?
-  puts "You didnt enter anything!\nExiting now.".yellow
-  exit
-end
-# Need to make sure that it is ONLY 1 word
+abort("You didnt enter anything!\nExiting now.".red) if user_input.empty?
 
 # Convert Word to an Array
 expanded_word = user_input.split(//)
-
-# Get the first letter of that word
 first = expanded_word.shift.yellow
-# Get the remaining letters of the word
 rest = expanded_word.join("").yellow
+
 # Put it all together and present it back to the user
 puts "\nYour word #{user_input} in Pig Latin would be: \n\n".red
 puts rest + first + "ay\n\n".yellow
