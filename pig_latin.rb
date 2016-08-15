f7e1f9fa4db7d8c4dbc1deebd631c66f9ab93222
Chase Hallsted <chase.hallsted@gmail.com>
2016-08-15 03:58:44 -0500

    Converted to single linebreaks

--- pig_latin.rb
+++ pig_latin.rb
@@ -8,7 +8,7 @@ require 'colorize' # Allows the use of colorized text to be printed to the scree
 # This program will convert a given string to Pig Laten
 # TODO: Need to make sure that it is ONLY 1 word
 
-puts "\n\nPlease give me a word, I will convert it to Pig Latin!\n\n".red
+puts "\nPlease give me a word, I will convert it to Pig Latin!\n".red
 user_input = gets.chomp.downcase
 
 # Complain if the user didnt enter anything
@@ -20,5 +20,5 @@ first = expanded_word.shift.yellow
 rest = expanded_word.join("").yellow
 
 # Put it all together and present it back to the user
-puts "\nYour word #{user_input} in Pig Latin would be: \n\n".red
-puts rest + first + "ay\n\n".yellow
+puts "\nYour word #{user_input} in Pig Latin would be: \n".red
+puts rest + first + "ay\n".yellow
